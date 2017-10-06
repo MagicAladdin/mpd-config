f77904b7a63efbb86c01ebf4907d5407339887b3
Sascha Wander <sascha.wander@gmail.com>
2017-10-06 05:57:45 +0200

    rm ympd

--- .mpd/launch.sh
+++ .mpd/launch.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
-killall -q ympd
-while pgrep -u $UID -x ympd >/dev/null; do sleep 1; done
-
 killall -q mpd
 while pgrep -u $UID -x mpd >/dev/null; do sleep 1; done
 
 
 mpd &
-
-ympd --host z800.gnu.local --user wandsas &
 
 # -*- mode: sh
 # vim: fenc=utf-8 ft=sh
