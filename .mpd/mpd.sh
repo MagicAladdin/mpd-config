bcd39a446841ba713ff8e6e607c891822dc0921e
Sascha Wander <sascha.wander@gmail.com>
2017-10-06 05:32:11 +0200

    foo

--- .mpd/mpd.sh
+++ .mpd/mpd.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-killall -q ympd
-while pgrep -u $UID -x ympd >/dev/null; do sleep 1; done
-
 killall -q mpd
 while pgrep -u $UID -x mpd >/dev/null; do sleep 1; done
 
-
 mpd &
+
+# -*- mode: sh -*-
+# vim: fenc=utf-8 ft=sh
+#!/bin/sh
+
+killall -q ympd
+while pgrep -u $UID -x ympd >/dev/null; do sleep 1; done
 
 ympd \
     --host z800.gnu.local \
+    --port 6600 \
     --webport 8080 \
     --user wandsas &
 
-# -*- mode: sh
+# -*- mode: sh -*-
 # vim: fenc=utf-8 ft=sh
