1390e1583c15ba79e16c8eb7d04635ec1da84752
Sascha Wander <sascha.wander@gmail.com>
2018-09-09 16:22:12 +0200

    hookupdated

--- .cfg-post.d/mpd-config.sh
+++ .cfg-post.d/mpd-config.sh
@@ -3,9 +3,10 @@
 #
 # Install or update youtube-dl
 #
-if [[ -f "/usr/local/bin/youtube-dl" ]]
-then 	youtube-dl -U
-else 	curl -fsSL https://yt-dl.org/downloads/latest/youtube-dl \
+if [[ -f "/usr/local/bin/youtube-dl" ]]; then
+	youtube-dl -U
+else
+	curl -fsSL https://yt-dl.org/downloads/latest/youtube-dl \
 		--create-dirs -o /usr/local/bin/youtube-dl
 	chmod a+rx /usr/local/bin/youtube-dl
 fi
